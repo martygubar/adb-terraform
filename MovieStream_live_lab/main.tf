9a3cc440f5ce4328cbd8fdb86af0d85ec0def925
martygubar <martin.gubar@oracle.com>
2023-09-26 17:30:31 -0400

    cleanup

--- MovieStream_live_lab/main.tf
+++
@@ -1,8 +0,0 @@
-# Copyright © 2023, Oracle and/or its affiliates.
-# All rights reserved. Licensed under the Universal Permissive License (UPL), Version 1.0 as shown at https://oss.oracle.com/licenses/upl.
-
-# Create ADW Database with Endpoint in private subnet
-module "adw_database_private_endpoint" {
-  source = "./modules/cloud-foundation-library/database/adw_private_endpoint_for_each"
-  adw_params = local.adw_params 
-}
---
+++ adb-select-ai-demo/outputs.tf
@@ -0,0 +1,48 @@
+# Copyright © 2023, Oracle and/or its affiliates.
+# All rights reserved. Licensed under the Universal Permissive License (UPL), Version 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+# Autonomous Database Outputs:
+
+output "adb_admin_password" {
+  description = "ADB Admin password"
+  value = var.db_password
+  sensitive = true
+}
+
+output "adb_user_name" {
+  description = "Workshop user name"
+  value = "MOVIESTREAM"
+}
+
+output "adb_user_password" {
+  description = "Workshop user initial password"
+  value = "watchS0meMovies#"
+}
+
+output "ADW_Database_db_connection" {
+  value = module.adw_ecpus.db_connection
+}
+
+output "ADW_Database_ip" {
+  value = module.adw_ecpus.private_endpoint_ip
+}
+
+output "Database_Actions" {
+  value = module.adw_ecpus.url
+}
+
+output "graph_studio_url" {
+  value = module.adw_ecpus.graph_studio_url
+}
+
+output "machine_learning_user_management_url" {
+  value = module.adw_ecpus.machine_learning_user_management_url
+}
+
+output "apex_url" {
+  value = module.adw_ecpus.apex_url
+}
+
+output "database_fully_qualified_name" {
+  value = module.adw_ecpus.database_fully_qualified_name
+}
